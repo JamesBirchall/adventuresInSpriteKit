636c19114bd2790cce4beaa840180343d6969f93
James Birchall <jamesbirchall@gmail.com>
2017-04-18 21:44:09 +0100

    Main Menu - updates
    Added swift file for the main menu scene file.

---
+++ JackTheGiant/JackTheGiant/MainMenuScene.swift
@@ -0,0 +1,17 @@
+//
+//  MainMenuScene.swift
+//  JackTheGiant
+//
+//  Created by James Birchall on 18/04/2017.
+//  Copyright © 2017 James Birchall. All rights reserved.
+//
+
+import SpriteKit
+
+class MainMenuScene: SKScene {
+    override func didMove(to view: SKView) {
+        super.didMove(to: view)
+        
+        print("View Shown..")
+    }
+}
