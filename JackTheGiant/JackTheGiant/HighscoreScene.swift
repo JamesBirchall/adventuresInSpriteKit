43c1ecdfba2a798a9bca5d9183aa15cc150ce96d
James Birchall <jamesbirchall@gmail.com>
2017-04-19 06:41:56 +0100

    fixed font issue
    :-)

--- JackTheGiant/JackTheGiant/HighscoreScene.swift
+++ JackTheGiant/JackTheGiant/HighscoreScene.swift
@@ -15,5 +15,20 @@ class HighscoreScene: SKScene {
 //        if let node1 = self.childNode(withName: "HighscoreLabel") as? SKLabelNode {
 //            node1.fontName = "blow"
 //        }
+        
+//        for family: String in UIFont.familyNames
+//        {
+//            print("\(family)")
+//            for names: String in UIFont.fontNames(forFamilyName: family)
+//            {
+//                print("== \(names)")
+//            }
+//        }
+        
+//        let font = UIFont(name: "Blow", size: 48)
+//        if let node1 = self.childNode(withName: "HighscoreLabel") as? SKLabelNode {
+//            node1.fontName = font?.fontName
+//            node1.fontSize = (font?.pointSize)!
+//        }
     }
 }
